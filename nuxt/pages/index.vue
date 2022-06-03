98f53e740c6bb2c3abe692d18722b092c570a0cc
acaibawl <hagiya0719@gmail.com>
2022-06-03 17:41:20 +0900

    Dockerを使ったLaravelとNuxtを連携するアプリの雛形

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title red">
+        {{ greet }} // デフォルトの「nuxt」から変更した
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData ({ app }) {
+    const greet = await app.$axios.$get('/').catch(err => console.log(err))
+    return { greet }
+  }
+})
+</script>
